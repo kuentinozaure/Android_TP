f900dc54b8d74b1fe67214f05d4865fc7a69c1bf
lil_kent1 <aubryquentin700@gmail.com>
2020-11-06 18:44:28 +0100

    adding changing page when i click on my service button

--- app/src/main/java/com/lil_kent1/partageservices/MainActivity.java
+++ app/src/main/java/com/lil_kent1/partageservices/MainActivity.java
@@ -1,6 +1,8 @@
 package com.lil_kent1.partageservices;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMyServices(View v) {
+        System.out.println("dfdf");
+
+        Intent i = new Intent(this, Myservice.class);
+        startActivity(i);
     }
 }
